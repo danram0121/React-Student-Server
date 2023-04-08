195e0714c7c49e65b5074d161926748bbbb54273
danram0121 <118145477+danram0121@users.noreply.github.com>
2023-04-07 22:38:36 -0400

    added procfile and send index

--- studentserver.js
+++ studentserver.js
@@ -188,4 +188,7 @@ app.get("/students", function (req, res) {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
